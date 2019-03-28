366733d74a32a3b21c6205e19ab5de1722142bf9
Anna Skoulikari <annaskoulikari@gmail.com>
2019-03-28 17:31:46 +0100

    updated routes

--- routes/movies.js
+++ routes/movies.js
@@ -1,10 +1,23 @@
 const express = require("express");
 const router = express.Router();
+const axios = require("axios");
 
 router.get(
-  "https://api.themoviedb.org/3/trending/movie/week?api_key=943d003becc08df50bf054b11efaccb1",
+  "/",
+
   (req, res) => {
-    res.send();
+    // res.send();
+
+    axios
+      .get(
+        // "https://api.themoviedb.org/3/trending/movie/week?api_key=943d003becc08df50bf054b11efaccb1"
+        "https://api.themoviedb.org/3/search/movie?api_key=943d003becc08df50bf054b11efaccb1&query=marvel"
+      )
+      .then(response => {
+        return response.data;
+      })
+      .then(response => res.send(response));
+
     console.log("you reached backend route");
   }
 );
