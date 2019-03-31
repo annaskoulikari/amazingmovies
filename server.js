924a10337827cf4464cac62de0336406ee7a83a0
Anna Skoulikari <annaskoulikari@gmail.com>
2019-04-01 00:26:19 +0200

    fixing db

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const port = process.env.PORT || 5000;
 //
 //Connect to MongoDB
 mongoose
-  .connect(process.env.MONGO_URI || db, {
+  .connect(db, {
     useNewUrlParser: true,
     useCreateIndex: true
   }) // Adding new mongo url parser
