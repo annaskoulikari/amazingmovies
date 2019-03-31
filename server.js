08ac4eea5437ef11a000e5563a237774e6da5b39
Anna Skoulikari <annaskoulikari@gmail.com>
2019-03-31 18:55:30 +0200

    preparing app to deploy

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const config = require("config");
 const mongoose = require("mongoose");
 
 const cors = require("cors");
+
+const path = require("path");
 const app = express();
 
 //DB Config
@@ -36,6 +38,16 @@ app.use("/tv", require("./routes/tv"));
 app.use("/auth", require("./routes/auth"));
 app.use("/users", require("./routes/users"));
 app.use("/favourites", require("./routes/favourites"));
+
+//serve static assets if we are in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
